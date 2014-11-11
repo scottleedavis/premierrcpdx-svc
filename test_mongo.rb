eacf5b4ff6d4acc27f999c1630e3b4e401fe941f
Scott Davis <scott.davis@webtrends.com>
2014-11-11 14:04:25 -0800

    added comment for starting mongo.

--- test_mongo.rb
+++ test_mongo.rb
@@ -2,6 +2,8 @@ require 'mongo'
 require 'bson'
 require 'pry'
 
+#mongod --dbpath ~/mongo-data/
+
 db = Mongo::Connection.new("localhost").db("mydb") 
 col = db.collection("testCollection")
 binding.pry
